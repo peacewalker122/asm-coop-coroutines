9dde892b52a24c59c8217c4b5995a4a0c9877c66
Daniel Anugerah <81625151+peacewalker122@users.noreply.github.com>
2025-09-17 20:07:36 +0800

    feat: complete implement of coroutine

--- include/context.h
+++ include/context.h
@@ -1,6 +1,7 @@
 #ifndef CONTEXT_H
 #define CONTEXT_H
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
@@ -20,6 +21,7 @@ typedef struct Context {
   coro_func_t fn;             // function to call on first run 64
   void *arg;                  // function arguements 72
   struct Context *return_ctx; // context to return to when coroutine finishes 80
+  uint64_t finished;          // has this context finished execution? 88
 } Context;
 
 // Implemented in assembly (we'll wire these later)
--- src/main.c
+++ src/main.c
@@ -10,10 +10,12 @@ void start_fn(void *p) {
   // sleep for few seconds
   for (int i = 0; i < 3; i++) {
     printf("[coro] running with arg: %s\n", (char *)p);
-    // simulate doing some work
     thrd_sleep(&(struct timespec){.tv_sec = 1}, NULL);
   }
+  co_yield();
+  thrd_sleep(&(struct timespec){.tv_sec = 5}, NULL);
 
+  printf("[coro] finished with arg: %s\n", (char *)p);
   puts("[coro] to main");
 }
 
@@ -34,6 +36,7 @@ void second_fn(void *p) {
   }
   fclose(file);
 
+  co_yield();
   printf("[coro2] finished reading file: %s\n", (char *)p);
   puts("[coro2] to main");
 }
@@ -44,9 +47,7 @@ int main(void) {
     return 1;
   }
 
-  co_t *coroutine1 =
-      co_spawn(start_fn, "Jokowi, baik hati dan bijaksana, senang membantu "
-                         "rakyatnya. Ucap seorang bapak-bapak dari fakfak.");
+  co_t *coroutine1 = co_spawn(start_fn, "wulululu");
   if (!coroutine1) {
     fprintf(stderr, "Failed to spawn coroutine\n");
     return 1;
@@ -57,8 +58,7 @@ int main(void) {
     return 1;
   }
 
-  co_join(coroutine1);
-  co_join(coroutine2);
+  co_run();
 
   puts("[main] back from coroutine");
   puts("[main] done");
